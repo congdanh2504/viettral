6b649cae9c77f16bdb1cbd15e4256adc9cbf269b
congdanh2504 <73818935+congdanh2504@users.noreply.github.com>
2021-06-18 23:12:10 +0700

    Update accMana.js

--- js/accMana.js
+++ js/accMana.js
@@ -5,7 +5,7 @@ let password = queries[1].substring(9);
 if (usernamee == "admin" && password == "admin"){
     let nav = document.getElementById("acc");
     console.log(nav.innerHTML);
-    nav.innerHTML += '<li role="separator" class="divider"></li><li><i class="fa fa-shield" aria-hidden="true" style="color: #F7559A;"></i><a href="../admin/statistical.html?username=admin&password=admin">Administration</a></li>'
+    nav.innerHTML += '<li role="separator" class="divider"></li><li><i class="fa fa-shield" aria-hidden="true" style="color: #F7559A;"></i><a href="./admin/statistical.html?username=admin&password=admin">Administration</a></li>'
 }
 let user = document.getElementById("user");
 user.innerHTML = " "+usernamee;
